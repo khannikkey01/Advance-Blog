fa986d0b0c514a0a135089c93c7a4e7aacbe27d5
khannikkey <khannikkey@email.com>
2023-08-30 22:00:07 +0530

    Blog with Advanced Feature

---
+++ blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from blog.models import Comment
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    to = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
+    
+    
+    
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class SearchForm(forms.Form):
+    query = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
